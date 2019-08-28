e84927146091ba73920c70b13385cefa16f8ac5b
Brenden Riley <BRiley001@west-mec.org>
2019-08-28 15:28:38 -0700

    basic stuff
    reworked some javascript, and added html and css to make it look nice.

--- game.js
+++ game.js
@@ -3,32 +3,58 @@
 
 
 //Global Variables
-var playerClass = "";
-var playerCurrentHP = 50;
-var playerMaxHP=50;
-var playerD = 3;
-var playerA = 3;
-var playerMana = 20;
 
-var enemyName = "";
-var enemyHP = 0;
-var enemyD = 0;
-var enemyA = 0
+var enemyName = "Dummy";
+var enemyHP = 1000;
+var enemyD = 10;
+var enemyA = 1;
 
+const hero = {
+    name: "You",
+    hp:50,
+    maxhp:50,
+    defense:3,
+    attack:3,
+    mana:20
+}
+
+window.onload=setup;
 
 //Functions
+
+function setup(){
+    document.querySelector("#attack").onclick = damageCalc;
+    document.querySelector("#magic").onclick = magic;
+    document.querySelector("#hPotion").onclick = a;
+    document.querySelector("#mPotion").onclick = a;
+}
 
 function randomNumber(size) {
     var number = Math.round(Math.random() * size);
     return number;
 }
 
-function damageCalc(attack, defense){
+function damageCalc(){
+    let attack = hero.attack;
+    let defense = enemyD;
     if (randomNumber(10) === 10) {
         var totalDamage = attack * 3;
+        console.log("CRIT");
     } else {
         var totalDamage = attack;
     }
-    totalDamage = Math.round(totalDamage - defense*(1/5));
+    totalDamage = Math.round(totalDamage - defense*(1/3));
+    if (totalDamage < 0) {
+        totalDamage=0
+    }
+    enemyHP-=totalDamage;
     console.log(totalDamage)
+}
+
+function updateStats(){
+
+}
+
+function magic(){
+
 }
